96e9f569df244c36b8b5a4477c8716847201ce99
Vadym <vadikdeputat@gmail.com>
2021-06-20 19:23:42 +0200

    init

---
+++ src/main/java/org/spring/bsa/BsaApplication.java
@@ -0,0 +1,13 @@
+package org.spring.bsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsaApplication.class, args);
+	}
+
+}
