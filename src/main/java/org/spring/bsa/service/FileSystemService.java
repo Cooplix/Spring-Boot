856c137531a088129897f59521c664c6143688a0
Vadym <vadikdeputat@gmail.com>
2021-06-21 18:39:37 +0200

    add method FileSystemService.copyGifToUserFolder

--- src/main/java/org/spring/bsa/service/FileSystemService.java
+++ src/main/java/org/spring/bsa/service/FileSystemService.java
@@ -3,10 +3,13 @@ package org.spring.bsa.service;
 import org.spring.bsa.entities.GifEntity;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.IIOException;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 
 @Service
 public class FileSystemService {
@@ -57,7 +60,19 @@ public class FileSystemService {
 		copyGifToUserFolder(userId, gifEntity.getQuery(), cacheGif.getPath());
 	}
 
-	private void copyGifToUserFolder(String userId, String query, String path) {
+	private File copyGifToUserFolder(String userId, String query, String cachePath) {
+		File source = new File(cachePath);
+		File destination = new File(PATH + "\\users\\" + userId + "\\" + query);
+
+		try {
+			destination.mkdirs();
+			destination = new File(destination, source.getName());
+			Files.copy(source.toPath(), destination.toPath());
+		} catch (IOException ex) {
+			System.out.println("File exist");
+		}
+
+		return new File(destination, source.getName());
 	}
 
 	private File getGifPath(GifEntity gifEntity) {
