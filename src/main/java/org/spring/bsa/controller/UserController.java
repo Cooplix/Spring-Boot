b625634dabe72e17dec929f4700cf66242be0605
Vadym <vadikdeputat@gmail.com>
2021-06-23 19:03:48 +0200

    add user history mapping

---
+++ src/main/java/org/spring/bsa/controller/UserController.java
@@ -0,0 +1,60 @@
+package org.spring.bsa.controller;
+
+import org.spring.bsa.dto.HistoryDto;
+import org.spring.bsa.entities.Cache;
+import org.spring.bsa.service.FileSystemService;
+import org.spring.bsa.service.GiphyService;
+import org.spring.bsa.service.ParserService;
+import org.spring.bsa.utils.UserUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+    GiphyService giphyService;
+    FileSystemService fileSystemService;
+    Cache cache;
+    UserUtil userUtil;
+    ParserService parserService;
+
+    public UserController(GiphyService giphyService, FileSystemService fileSystemService, Cache cache, UserUtil userUtil, ParserService parserService) {
+        this.giphyService = giphyService;
+        this.fileSystemService = fileSystemService;
+        this.cache = cache;
+        this.userUtil = userUtil;
+        this.parserService = parserService;
+    }
+    private ResponseEntity<?> validateUserID(String id) {
+        if(userUtil.checkUserID(id)) {
+            Map<String, String> tempResponse = new HashMap<>();
+            tempResponse.put("message", "Invalid request");
+            return new ResponseEntity<>(tempResponse, HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+
+    @GetMapping("{user_id/history}")
+    public ResponseEntity<?> getUserHistory(@PathVariable String id) {
+        if(validateUserID(id) != null) {
+            return validateUserID(id);
+        }
+
+        HistoryDto[] result = parserService.parseHistory(fileSystemService.getHistory(id));
+
+        if(result != null) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+}
